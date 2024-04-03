d6a20c0e1ce4ec8463d7fb1f96d0ae436a069958
nandkishorr <rachulr7@gmail.com>
2024-04-04 00:18:34 +0530

    service function for create hostorylogs completed

--- src/services/healthlogs.service.js
+++ src/services/healthlogs.service.js
@@ -8,3 +8,28 @@ const getAllHealthLogs = async () => {
         throw new Error('Error fetching user');
     }
 }
+
+const createHealthLog = async (req) =>  {
+    try {
+        const isEmptyReq = Object.values(req).every(value => value === '');
+
+        if (isEmptyReq) {
+            return {
+                message: "Health log details cannot be empty"
+            };
+        }
+        const existingHealthLog = await HealthLogModel.findOne({...req});
+        
+        if (existingHealthLog) {
+            return {
+                message:"Health log already exists in the database"
+            };
+        } else {
+            const newHealthLog = new HealthLogModel({...req});
+            return await newHealthLog.save();
+        }
+    } catch (error) {
+        console.error('Error creating health log:', error);
+        throw error;
+    }
+};
