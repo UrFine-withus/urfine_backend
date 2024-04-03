4abead513afdc0e94a1643f66619e6b5a55e547f
nandkishorr <rachulr7@gmail.com>
2024-04-03 23:38:58 +0530

    controller for create history logs

--- src/controllers/healthlogs.controller.js
+++ src/controllers/healthlogs.controller.js
@@ -12,3 +12,15 @@ const getAllHealthLogsData = async (req, res) => {
 }
 
 
+const createHealthLogData = async (req, res) => {
+    try {
+        console.log(req.body);
+        const healthlogs = await createHealthLog(req.body);
+        console.log(healthlogs);
+        res.send(healthlogs);
+    } catch (error) {
+        console.error('Error fetching healthlogs:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+
+}   
