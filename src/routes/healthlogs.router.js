97adc0c07675be74911adcf941d5b8e1765877eb
nandkishorr <rachulr7@gmail.com>
2024-04-04 20:02:38 +0530

    api for user health logs dates initialized

--- src/routes/healthlogs.router.js
+++ src/routes/healthlogs.router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const{getAllHealthLogsData,createHealthLogData,updateHealthLogData,deleteHealthLogData,getUserHealthLogsData} = require('../controllers/healthlogs.controller');
+const{getAllHealthLogsData,createHealthLogData,updateHealthLogData,deleteHealthLogData,getUserHealthLogsData, getHealthLogdateData} = require('../controllers/healthlogs.controller');
 
 router.get('/healthlog', getAllHealthLogsData);
 router.get('/healthlog/user', getUserHealthLogsData);
+router.get('/healthlog/git dates', getHealthLogdateData);
 router.post('/healthlog', createHealthLogData);
 router.put('/healthlog', updateHealthLogData);
 router.delete('/healthlog', deleteHealthLogData);
