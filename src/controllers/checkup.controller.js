af279ea895b453709ef07729eb3464ab6b282eae
nandkishorr <rachulr7@gmail.com>
2024-04-11 00:52:27 +0530

    checkup create checkup  controller api initialized and completed

--- src/controllers/checkup.controller.js
+++ src/controllers/checkup.controller.js
@@ -10,7 +10,19 @@ const getAllCheckupData = async (req, res) => {
       res.status(500).json({ error: 'Internal Server Error' });
     }
   }
+    const createCheckupData = async (req, res) => {
+        try {
+        const checkup = await createCheckup();
+        console.log(checkup);
+        res.send(checkup);
+        } catch (error) {
+        console.error('Error fetching checkup:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+        }
+    }
+    
 
   module.exports = {
-    getAllCheckupData
+    getAllCheckupData,
+    createCheckupData
   }
--- src/services/checkup.service.js
+++ src/services/checkup.service.js
@@ -2,7 +2,7 @@ const {CheckupModel} = require('../models');
 
 const getAllCheckup = async (req, res) => {
     try {
-        const checkup = await CheckupModel.find();
+        const checkup = await CheckupModel.find({deleted: null});
         console.log(checkup);
         res.send(checkup);
     } catch (error) {
