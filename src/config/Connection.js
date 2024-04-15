7a53c32a03b81393b7d6555a0a907c61886a82d1
nandkishorr <rachulr7@gmail.com>
2024-04-16 03:15:42 +0530

    updated the userfile api and databse storing is not integrated

--- src/config/Connection.js
+++ src/config/Connection.js
@@ -1,17 +1,38 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
-;
+const dotenv = require('dotenv');
+
 async function connectToDb() {
     try {
+        // Load environment variables
+        dotenv.config();
+        
+        // Get MongoDB URI from environment variables
         const dbUri = process.env.MONGODB_URL;
-        await mongoose.connect(dbUri, {
-            dbName: 'Urfine',
 
+        // Connect to MongoDB
+       const connection = await mongoose.connect(dbUri, {
+            dbName: 'Urfine', 
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
         });
-
-        console.log('Connected to MongoDB');
+        console.log('Connected to MongoDB ');
+        return connection;
     } catch (error) {
-        console.error('Error connecting to MongoDB', error);
+        console.error('Error connecting to MongoDB', error);
+        // You might want to handle the error further, depending on your application's requirements
+    }
 }
-}
+
+// Close MongoDB connection when the Node.js process terminates
+process.on('SIGINT', async () => {
+    try {
+        await mongoose.connection.close();
+        console.log('MongoDB connection closed');
+        process.exit(0);
+    } catch (error) {
+        console.error('Error closing MongoDB connection', error);
+        process.exit(1);
+    }
+});
+
 module.exports = connectToDb;
