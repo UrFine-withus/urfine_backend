149688e8c79dfe6d14a6a629e47d61e3e6d9e60b
nandkishorr <rachulr7@gmail.com>
2024-04-16 16:43:45 +0530

    userinfo routes updated

--- src/routes/userInfo.router.js
+++ src/routes/userInfo.router.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const { addUserInfoData,getUserInfoData,updateUserInfoData}  = require('../controllers/userInfo.controller');
 
 // Define your routes
-router.post('/userinfo',addUserInfoData);
-router.get('/userinfo',getUserInfoData);
-router.put('/userinfo',updateUserInfoData);
+router.route('/')
+     .post(addUserInfoData)
+     .get(getUserInfoData)
+     .put(updateUserInfoData);
 // Add more routes as needed
 
 module.exports = router;
