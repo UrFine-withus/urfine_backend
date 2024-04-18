0f4e9829cca2cbb3cc5e6a34009b4cf1f11ef1eb
nandkishorr <rachulr7@gmail.com>
2024-04-19 04:12:13 +0530

    api completed

--- src/routes/healthlogs.router.js
+++ src/routes/healthlogs.router.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-// const{getAllHealthLogsData,createHealthLogData,updateHealthLogData,deleteHealthLogData} = require('../controllers/healthlogs.controller');
+const{getAllHealthLogsData,createHealthLogData,updateHealthLogData,deleteHealthLogData} = require('../controllers/healthlogs.controller');
 
-// router.route('/')
-// .get(getAllHealthLogsData)
-// .post(createHealthLogData)
-// .put(updateHealthLogData)
-// .delete(deleteHealthLogData)
+router.route('/')
+.get(getAllHealthLogsData)
+.post(createHealthLogData)
+.put(updateHealthLogData)
+.delete(deleteHealthLogData)
 
 module.exports = router;
