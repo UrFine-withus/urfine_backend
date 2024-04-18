b38bc776da0e2b439d2ec56bb79681cbddc90987
NANDKISHOR R <96693626+nandkishorr@users.noreply.github.com>
2024-04-18 11:58:14 +0000

    healthlog model

--- src/models/index.js
+++ src/models/index.js
@@ -3,10 +3,13 @@ const UserInfoModel = require('./userInfo.model');
 const EmergencyInfoModel = require('./emergencyInfo.model');
 const HistoryLogModel = require('./historylogs.model');
 const CheckupModel = require('./checkup.model');
+const HealthlogModel=require('./healthlogs.model')
 module.exports = {
     UserDataModel,
     UserInfoModel,
     EmergencyInfoModel,
     HistoryLogModel,
-    CheckupModel
+    CheckupModel,
+    HealthlogModel
+
 };
