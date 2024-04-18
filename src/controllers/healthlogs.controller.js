cf3e3bbe2b5ffedc1c761675574041515a33f7bf
nandkishorr <rachulr7@gmail.com>
2024-04-19 02:05:53 +0530

    create HealthLogsData controller created

--- src/controllers/healthlogs.controller.js
+++ src/controllers/healthlogs.controller.js
@@ -8,3 +8,19 @@ const getAllHealthLogsData = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 }
+
+const createHealthLogData = async (req, res) => {
+    try {
+        const healthlog = req.body;
+        const newHealthLog = await createHealthLog(healthlog);
+        res.status(201).json(newHealthLog);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+
+}
+
+module.exports = {
+    getAllHealthLogsData,
+    createHealthLogData
+}
