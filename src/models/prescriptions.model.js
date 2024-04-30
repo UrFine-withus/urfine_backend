a9472971ddac43d619889d39c6ca77dfea583b98
nandkishorr <rachulr7@gmail.com>
2024-05-01 01:03:42 +0530

    prescription upload model

---
+++ src/models/prescriptions.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const prescriptionSchema = new mongoose.Schema({
+    _userID:{
+        type:String,
+        required:true,
+    },
+    prescription_image_url:{
+        type: [String],
+        required: true,
+        default: []
+      },
+      
+    upload_month:{
+        type: String,
+        required: true,
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Prescription = mongoose.model('Prescription', prescriptionSchema);
+module.exports = Prescription;
