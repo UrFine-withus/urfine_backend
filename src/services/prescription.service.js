34ab74def4feed2d4675c8f5c5be173c6359b109
nandkishorr <rachulr7@gmail.com>
2024-05-03 12:58:43 +0530

    api for getting user prescription is completed

--- src/services/prescription.service.js
+++ src/services/prescription.service.js
@@ -19,10 +19,15 @@ const createPrescription = async (req) => {
         const currentMonth = currentDate.toLocaleString('default', { month: 'long' });
         const upload_month=currentMonth;
 
-        console.log(upload_month);
+        // console.log(upload_month);
         const prescription = new PrescriptionModel({upload_month,...req});
         await prescription.save();
-        return prescription;
+        if(prescription){
+            
+            return {
+                message: 'Prescription created successfully'
+            }
+         }
     } catch (error) {
         console.error('Error creating Prescription:', error);
         throw new Error('Error creating Prescription');
@@ -44,9 +49,22 @@ const deletePrescription = async (prescription_id) => {
     }
 
 }
+
+const getUserPrescription = async (_userID) => {
+    try {
+        const prescription = await PrescriptionModel.find({_userID}).sort({createdAt: -1});
+        if (prescription) {
+            return {prescription};
+        }
+    } catch (error) {
+        console.error('Error fetching Prescription:', error);
+        throw new Error('Error fetching Prescription');
+    }
+}
 module.exports = {
     getPrescription,
     createPrescription,
-    deletePrescription
+    deletePrescription,
+    getUserPrescription
 };
 
