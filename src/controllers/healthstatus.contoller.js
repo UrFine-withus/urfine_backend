0c4b14bf39c657017511fbdbcd3790eeac7040cc
nandkishorr <rachulr7@gmail.com>
2024-05-08 13:29:15 +0530

    health page setuped

--- src/controllers/healthstatus.contoller.js
+++ src/controllers/healthstatus.contoller.js
@@ -1,18 +1,16 @@
+const connectToDb =require('../config/Connection') 
 
-const getHealthData = async () => {
-    template=`<div>
-    <div>Welcome to Urfine</div>
-    <div>The server is Live.</>
-    </div>`
-    return template
-}
 
-const getHealthcheckData=async ()=>{
+const getHealthData = async (req, res) => {
+    const template = `<div style={{ textAlign: 'center' }}>
+        <h1>Welcome to Urfine</h1>
+        <h4>The server is Live.</h4>
+    </div>`;
+    res.send(template);
+};
 
-}
+
 module.exports = {
     getHealthData,
-    getHealthcheckData
-}
-
+};
 
--- src/routes/healthstatus.router.js
+++ src/routes/healthstatus.router.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const {getHealthcheckData,getHealthData} =require('../controllers/healthstatus.contoller')
-router.route('/healthz').get(getHealthcheckData);
 router.route('/').get(getHealthData)
 
 
